c0c9729aff4d35bd8958b1d5fcd5aa7dcace1a5c
ARSHEET <75607915+ARSHEET@users.noreply.github.com>
2021-09-02 11:14:07 +0530

    Add files via upload

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.css"
+
+export default function Header() {
+    return (
+        <div className="header">
+            <div className="headerTitles">
+                <span className="headerTitleSm">React & Node</span>
+                <span className="headerTitleLg">Blog</span>
+            </div>
+            <img className="headerImg" src="http://1.bp.blogspot.com/-XG2GXoOjreM/UqaioSAS79I/AAAAAAAAPFg/6qPzZIFCZ7A/s1600/spring-desktop-backgrounds-wallpapers.jpg" alt="" />
+        </div>
+    )
+}
